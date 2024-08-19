daa80b0905a253f798c4034ad9ef2135edee9901
Prasanna Achar <95010652+PRASANNAACHAR@users.noreply.github.com>
2024-08-19 18:09:16 +0530

    Add files via upload

---
+++ To-do-list/script.js
@@ -0,0 +1,36 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBox.value === '') {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"; // Unicode for multiplication sign (×)
+    span.className = "delete-btn";
+    li.appendChild(span);
+  }
+  inputBox.value = '';
+  saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.className === "delete-btn") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
